0015b8a0081038ad9645fda0ba18bddf40f22b75
Kondo Uchio <udzura@udzura.jp>
2019-02-21 18:28:02 +0900

    test commit 1 (#1)

--- test.rb
+++ test.rb
@@ -2,12 +2,6 @@ require 'database'
 
 data = Database.new
 
-puts "are you admin? [y/N]"
-unless gets.chomp == 'y'
-  puts "canceled"
-  exit 0
-end
-
 puts "enter your name"
 name = gets.chomp
 puts "enter your password"
